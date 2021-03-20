72c62c1d361d28196b6e97037125190363d73e96
Jonny Lin <jonnylin@ubermedia.com>
2021-03-20 14:14:25 -0700

    Added more tests for packet.test.js

--- packages/net/packet.js
+++ packages/net/packet.js
@@ -16,7 +16,7 @@ class Parser {
   /**
    * Reads a byte
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   byte() {
     const byte = this.data.readIntLE(this.offset, 1);
@@ -27,7 +27,7 @@ class Parser {
   /**
    * Reads a boolean
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   bool() {
     const bool = this.readByte();
@@ -37,7 +37,7 @@ class Parser {
   /**
    * Reads an unsigned byte
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   ubyte() {
     const uByte = this.data.readUIntLE(this.offset, 1);
@@ -48,7 +48,7 @@ class Parser {
   /**
    * Reads a short
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   short() {
     const uByte = this.data.readUIntLE(this.offset, 1);
@@ -59,7 +59,7 @@ class Parser {
   /**
    * Reads an unsigned short
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   ushort() {
     const uShort = this.data.readUInt16LE(this.offset);
@@ -70,7 +70,7 @@ class Parser {
   /**
    * Reads a character
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   char() {
     this.parsed.push(String.fromCharCode(97 + this.readShort()));
@@ -79,7 +79,7 @@ class Parser {
   /**
    * Reads an integer
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   int() {
     const int = this.data.readInt32LE(this.offset);
@@ -90,7 +90,7 @@ class Parser {
   /**
    * Reads an unsigned integer
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   uint() {
     const uInt = this.data.readUInt32LE(this.offset);
@@ -101,7 +101,7 @@ class Parser {
   /**
    * Reads a long
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   long() {
     const long = this.data.readBigInt64LE(this.offset);
@@ -112,7 +112,7 @@ class Parser {
   /**
    * Reads an unsigned long
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   ulong() {
     const uLong = this.data.readBigUInt64LE(this.offset);
@@ -123,7 +123,7 @@ class Parser {
   /**
    * Reads a double
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   double() {
     const double = this.data.readDoubleLE(this.offset);
@@ -135,7 +135,7 @@ class Parser {
    * Reads an ASCII string
    * @method
    * @param {number} length Length of the string
-   * @return {PacketParser}
+   * @return {Parser}
    */
   ascii(length) {
     const stringBuffer = Buffer.alloc(length);
@@ -148,7 +148,7 @@ class Parser {
   /**
    * Reads a null terminated ASCII string
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   nullascii() {
     const stringArr = [];
@@ -163,7 +163,7 @@ class Parser {
   /**
    * Reads a MapleStory ASCII string
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   mapleascii() {
     const length = this.readShort();
@@ -173,7 +173,7 @@ class Parser {
   /**
    * Reads a position (x, y)
    * @method
-   * @return {PacketParser}
+   * @return {Parser}
    */
   pos() {
     const x = this.readShort();
@@ -184,7 +184,7 @@ class Parser {
    * Reads a specified length
    * @method
    * @param {number} length
-   * @return {PacketParser}
+   * @return {Parser}
    */
   read(length) {
     const ret = Buffer.alloc(length);
@@ -198,7 +198,7 @@ class Parser {
    * Skips the cursor a specified length
    * @method
    * @param {number} length
-   * @return {PacketParser}
+   * @return {Parser}
    */
   skip(length) {
     this.offset += length;
@@ -208,11 +208,220 @@ class Parser {
    * Moves the cursor to a specified byte offset
    * @method
    * @param {number} offset
-   * @return {PacketParser}
+   * @return {Parser}
    */
   seek(offset) {
     this.offset = offset;
     return this;
   }
+  /**
+   * Collects the parsed values and maps them to the input array strings
+   * @method
+   * @param {Array<string>} fieldNames
+   * @return {Map<string, ?>}
+   */
+  collect(fieldNames) {
+    if (fieldNames.length !== this.parsed.length) return false;
+    const ret = {};
+    const length = fieldNames.length;
+    for (let i = 0; i < length; i++) {
+      ret[fieldNames[i]] = this.parsed[i];
+    }
+    return ret;
+  }
 }
-module.exports = {Parser};
+/**
+ * Writes packets in little endian format
+ * @class
+ */
+class Writer {
+  /**
+   * @constructor
+   * @param {number} length
+   */
+  constructor(length = 32) {
+    this.data = Buffer.alloc(length);
+    this.bytesWritten = 0;
+  }
+  /**
+   * Used to allocated more bytes to the Buffer
+   * @method
+   * @param {number} byteSize Total byte length required
+   */
+  dynamicAlloc(byteSize) {
+    const bytesAvailable = this.data.length - this.bytesWritten;
+    if (bytesAvailable < byteSize) {
+      const bytesNeeded = byteSize - bytesAvailable;
+      this.data = Buffer.alloc(this.data.length + bytesNeeded, this.data);
+    }
+  }
+  /**
+   * Writes a byte
+   * @method
+   * @param {number} byte
+   * @return {Writer}
+   */
+  byte(byte) {
+    this.dynamicAlloc(1);
+    this.data.writeIntLE(byte, this.bytesWritten, 1);
+    this.bytesWritten += 1;
+    return this;
+  }
+  /**
+   * Writes an unsigned byte
+   * @method
+   * @param {number} uByte
+   * @return {Writer}
+   */
+  ubyte(uByte) {
+    this.dynamicAlloc(1);
+    this.data.writeUIntLE(uByte, this.bytesWritten, 1);
+    this.bytesWritten += 1;
+    return this;
+  }
+  /**
+   * Writes a short
+   * @method
+   * @param {number} short
+   * @return {Writer}
+   */
+  short(short) {
+    this.dynamicAlloc(2);
+    this.data.writeInt16LE(short, this.bytesWritten);
+    this.bytesWritten += 2;
+    return this;
+  }
+  /**
+   * Writes an unsigned short
+   * @method
+   * @param {number} uShort
+   * @return {Writer}
+   */
+  ushort(uShort) {
+    /* Lol u short */
+    this.dynamicAlloc(2);
+    this.data.writeUInt16LE(uShort, this.bytesWritten);
+    this.bytesWritten += 2;
+    return this;
+  }
+  /**
+   * Writes an integer
+   * @method
+   * @param {number} int
+   * @return {Writer}
+   */
+  int(int) {
+    this.dynamicAlloc(4);
+    this.data.writeInt32LE(int, this.bytesWritten);
+    this.bytesWritten += 4;
+    return this;
+  }
+  /**
+   * Writes an unsigned integer
+   * @method
+   * @param {number} uInt
+   * @return {Writer}
+   */
+  uint(uInt) {
+    this.dynamicAlloc(4);
+    this.data.writeUInt32LE(uInt, this.bytesWritten);
+    this.bytesWritten += 4;
+    return this;
+  }
+  /**
+   * Writes a long
+   * @method
+   * @param {bigint} long
+   * @return {Writer}
+   */
+  long(long) {
+    this.dynamicAlloc(8);
+    this.data.writeBigInt64LE(long, this.bytesWritten);
+    this.bytesWritten += 8;
+    return this;
+  }
+  /**
+   * Writes an unsigned long
+   * @method
+   * @param {bigint} uLong
+   * @return {Writer}
+   */
+  ulong(uLong) {
+    this.dynamicAlloc(8);
+    this.data.writeBigUInt64LE(uLong, this.bytesWritten);
+    this.bytesWritten += 8;
+    return this;
+  }
+  /**
+   * Writes a Buffer
+   * @method
+   * @param {Buffer} buf
+   * @return {Writer}
+   */
+  write(buf) {
+    for (const byte of buf) this.writeByte(byte);
+    return this;
+  }
+  /**
+   * Writes an ASCII string
+   * @method
+   * @param {string} str
+   * @return {Writer}
+   */
+  ascii(str) {
+    this.write(Buffer.from(str, 'utf-8'));
+    return this;
+  }
+  /**
+   * Writes a null-terminated ASCII string
+   * @method
+   * @param {string} str
+   * @return {Writer}
+   */
+  nullascii(str) {
+    this.ascii(str);
+    this.byte(0);
+    return this;
+  }
+  /**
+   * Writes a MapleStory ASCII string
+   * @method
+   * @param {string} str
+   * @return {Writer}
+   */
+  mapleascii(str) {
+    this.short(str.length);
+    this.ascii(str);
+    return this;
+  }
+  /**
+   * Writes a position (x, y)
+   * @method
+   * @param {Point} pos
+   * @return {Writer}
+   */
+  pos(pos) {
+    this.short(pos.x);
+    this.short(pos.y);
+    return this;
+  }
+  /**
+   * Writes a boolean value
+   * @method
+   * @param {boolean} bool
+   * @return {Writer}
+   */
+  bool(bool) {
+    this.byte(bool ? 1 : 0);
+    return this;
+  }
+  /**
+   * Returns the packet Buffer
+   * @method
+   * @return {Buffer}
+   */
+  buffer() {
+    return this.data;
+  }
+}
+module.exports = {Parser, Writer};
--- packages/net/test/packet.test.js
+++ packages/net/test/packet.test.js
@@ -1,9 +1,45 @@
 const assert = require('assert');
 const Packet = require('../packet');
-describe('packet.js Parser', function() {
-  it('should read a byte', function() {
-    const packet = new Packet.Parser(Buffer.from([0x2]));
-    const byte = packet.byte().parsed[0];
-    assert.strictEqual(byte, 2);
+describe('titan.Packet.Parser', function() {
+  it('should read a byte, ubyte', function() {
+    const packet = new Packet.Parser(Buffer.from([0x2, 0x2]));
+    const data = packet.byte().ubyte().collect(['val1', 'val2']);
+    assert.strictEqual(data.val1, 0x2);
+    assert.strictEqual(data.val2, 0x2);
+  });
+  it('should read a few bytes and collect', function() {
+    const packet = new Packet.Parser(Buffer.from([0x1, 0x2, 0x3]));
+    const fields = ['test1', 'test2', 'test3'];
+    const res = packet.byte().byte().byte().collect(fields);
+    assert.strictEqual(res.test1, 1);
+    assert.strictEqual(res['test2'], 2);
+    assert.strictEqual(res['test3'], 3);
+  });
+});
+describe('titan.Packet.Writer', function() {
+  it('should write a byte, ubyte', function() {
+    const packet = new Packet.Writer(2);
+    const buf = packet.byte(2).ubyte(1).buffer();
+    assert.strictEqual(buf.compare(Buffer.from([0x2, 0x1])), 0);
+  });
+  it('should write a short, ushort', function() {
+    const packet = new Packet.Writer(4);
+    const buf = packet.short(255).ushort(255).buffer();
+    assert.strictEqual(buf.compare(Buffer.from([0xff, 0, 0xff, 0])), 0);
+  });
+  it('should write an int, uint', function() {
+    const packet = new Packet.Writer(8);
+    const buf = packet.int(-2147483648).uint(2147483647).buffer();
+    const answer = Buffer.from([0, 0, 0, 0x80, 0xff, 0xff, 0xff, 0x7f]);
+    assert.strictEqual(buf.compare(answer), 0);
+  });
+  it('should write a long, ulong', function() {
+    const packet = new Packet.Writer(16);
+    const buf = packet.long(BigInt(2)).ulong(BigInt(2)).buffer();
+    const answer = Buffer.from([
+      0x2, 0, 0, 0, 0, 0, 0, 0,
+      0x2, 0, 0, 0, 0, 0, 0, 0
+    ]);
+    assert.strictEqual(buf.compare(answer), 0);
   });
 });
